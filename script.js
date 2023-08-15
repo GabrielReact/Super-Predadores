9a3b3fdf9679e52a2f885a80f152c2bf352fcbdd
Gabriel Carvalho <wityzaip.script@gmail.com>
2023-08-15 11:57:50 -0300

    First Comit
    Upando o Html e CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+const animais = document.getElementById('animais')
+
+const gridSection = document.getElementsByTagName('grid-section')
+
+const animaisAll = document.querySelectorAll('.animais img');
