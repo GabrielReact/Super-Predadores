48829f1118716663858296894bd9b4f859bea181
Gabriel Carvalho <wityzaip.script@gmail.com>
2023-08-23 00:15:05 -0300

    Algumas animaçôes
    Adicionei algumas animações, como a do scroll suave e esconder e mostrar o testo do Faq

--- script.js
+++ script.js
@@ -1,20 +1,96 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function startTab() {
 
-if (tabMenu.length && tabContent.length) {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+    if (tabMenu.length && tabContent.length) {
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
+    }
+}
+startTab();
+
+function startAcordion() {
+
+    const accordionList = document.querySelectorAll('.js-acordion dt');
+    const activeName = 'ativo';
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeName);
+        accordionList[0].nextElementSibling.classList.add(activeName);
+
+        function activeAcordion() {
+            this.classList.toggle(activeName);
+            this.nextElementSibling.classList.toggle(activeName);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAcordion);
+        });
+    }
+}
+startAcordion();
+
+function scrollSuave() {
+
+    const insadeLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const linkHref = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(linkHref);
+        // const topo = section.offsetTop;
+
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: 'start',
+        });
+
+        // window.scrollTo({  FORMA ALTERNATIVA
+        //     top: topo,
+        //     behavior: "smooth",
+        // });
+
     }
 
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-
+    insadeLinks.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
     });
+}
+scrollSuave();
+
+
+function scrollAnimation() {
+
+
+    const sections = document.querySelectorAll('.js-scroll');
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.7;
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+                // else 
+                // section.classList.remove('ativo');
+            })
+        }
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
 
 }
+scrollAnimation();
